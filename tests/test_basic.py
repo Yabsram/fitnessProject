2f38b83913a25bdf458f8e6c1fb7815f46b0c611
Mateo Viteri <mateoviteri13579@gmail.com>
2025-07-16 17:23:35 +0000

    Changed test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -13,20 +13,28 @@ class BasicTests(unittest.TestCase):
     #### tests ####
     ###############
 
-    def test_pages(self):
+    def test_home_page(self):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
         response = self.app.get('/home', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
+    
+    def test_workouts_page(self):
         response = self.app.get('/workouts', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
+
+    def test_recipes_page(self):
         response = self.app.get('/recipes', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
+
+    def test_login_page_and_logout(self):
         response = self.app.get('/login', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
-        response = self.app.get('/register', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
         response = self.app.get('/logout', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_page(self):
+        response = self.app.get('/register', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
 if __name__ == "__main__":
