673a2a7caeef27df049b4a4d787bda9e0bb7ab72
Mateo Viteri <mateoviteri13579@gmail.com>
2025-07-17 22:00:02 +0000

    tests fixed fr now

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -7,6 +7,8 @@ class BasicTests(unittest.TestCase):
 
     # executed prior to each test
     def setUp(self):
+        app.config['TESTING'] = True
+        app.config['SECRET_KEY'] = 'test-secret'
         self.app = app.test_client()
 
     ###############
